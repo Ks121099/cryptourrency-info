447438802a09ad67203fe5efbbb9a2d40d5ee822
Ks121099 <kunalsinha_bt2k17@dtu.ac.in>
2019-06-14 02:44:50 +0530

    Bitcoin functionality implemented

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const { app, BrowserWindow, Menu} = require('electron')
 const path=require('path')
 const url= require('url')
 const shell = require('electron').shell
-
+const ipc=require('electron').ipcMain
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
@@ -76,4 +76,8 @@ app.on('activate', () => {
   if (win === null) {
     createWindow()
   }
+})
+
+ipc.on('update-notify-value', function(event,arg){
+  win.webContents.send('targetpriceval',arg)
 })
--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
 const electron = require('electron')
 const path = require('path')
 const BrowserWindow = electron.remote.BrowserWindow
+const axios= require('axios')
+const ipc=electron.ipcRenderer
 
 const notifyBtn = document.getElementById('notif')
+const price=document.getElementById('price')
+const target=document.getElementById('targetprice')
+var targetpriceval
+
+const notification={
+title:'BitCoin Alert',
+body:'BTC just beat your target price'
+}
+
+function btc(){
+    axios.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD')
+        .then(res =>{
+        const cryptos= res.data.USD;
+        price.innerHTML='$'+cryptos.toLocaleString('en')
+        
+        if(targetprice.innerHTML!='' && targetpriceval <= res.data.USD){
+            var Notification=new Notification(notification.title,notification)
+        }
+    })
+}
+btc();
+setInterval(btc,10000);
 
 notifyBtn.addEventListener('click', function(event) {
     const modalPath = path.join('file://', __dirname, 'add.html')
-    let win = new BrowserWindow({   frame: false, transparent: true, width: 400, height: 200})
+    let win = new BrowserWindow({  
+        webPreferences: {
+            nodeIntegration: true
+          },
+           frame: false, transparent: true, width: 400, height: 200})
     win.on('close', function() { win = null })
     win.loadURL(modalPath)
     win.show()
+})
+
+ipc.on('targetpriceval' ,function(event,arg){
+    targetpriceval= Number(arg)
+    targetprice.innerHTML='$'+targetpriceval.toLocaleString('en')
 })
